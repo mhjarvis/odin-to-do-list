1447916cba1c47e3420dbbfea8b915270ed64791
Markus <markus.h.jarvis@gmail.com>
2022-10-10 15:59:39 -0500

    Add logic to display project element

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-console.log("test");
+let projects = ['default', 'project1'];
 
+let createProjectSection() {
+    const getProjectContainer = document.querySelector(".project-container");
 
-console.log("test again");
+    for(let i = 0; i <= projects.length; i++) {
+        const div = document.createElement('div');
+        div.innerHTML = projects[0];
+        getProjectContainer.appendChild(div);
+    }
+}
