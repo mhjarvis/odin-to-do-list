67bb060bd7e3aa818fcabe302c2d5ca4412935b7
Markus <markus.h.jarvis@gmail.com>
2022-10-15 16:35:15 -0500

    Remove previous code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,2 @@
 import { getProjects } from "./scripts/ToDoList";
 
-
-
-
-
-
-
-
-/* function createProjectSection() {
-    const getProjectContainer = document.querySelector(".project-container");
-
-    for(let i = 0; i < projects.length; i++) {
-        const div = document.createElement('div');
-        div.innerHTML = projects[i];
-        div.id = "project-" + i;
-        getProjectContainer.appendChild(div);
-        console.log(i);
-    }
-}; */
-
-/* createProjectSection();
- */
