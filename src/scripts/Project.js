df14ec0a0054a071f7d0684d7785b292385d8727
Markus <markus.h.jarvis@gmail.com>
2022-10-15 16:59:09 -0500

    Add function addTaskToCurrentProject()

--- src/scripts/Project.js
+++ src/scripts/Project.js
@@ -1,3 +1,4 @@
+import { displayProjects } from './ToDoList'
 
 class Project {
     constructor(title, tasks) {
@@ -7,9 +8,18 @@ class Project {
 }
 
 let allProjects = [];
+let currentProject = allProjects[0];
 
 addProject('First', ["Eat Bfast", "Eat Dinner"]);
 addProject('Second', ["Eat Something", "Eat Nothing"]);
+
+console.log(allProjects);
+
+function addTaskToCurrentProject(task) {
+    console.log(currentProject.tasks);
+    currentProject.tasks.push(task);
+    displayProjects();
+}
 
 function addProject(title, tasks) {
     let temp = new Project(title, tasks);
@@ -22,4 +32,4 @@ function getProjects() {
 
 
 
-export { getProjects }
+export { getProjects, addTaskToCurrentProject }
