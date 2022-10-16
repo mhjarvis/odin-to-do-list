2aede14145c4f4fa753b3a84e9a90d752fcd3c09
Markus <markus.h.jarvis@gmail.com>
2022-10-15 19:08:29 -0500

    Create EventControl.js; add eventListener for the Add Project button and export

--- src/scripts/EnventControl.js
+++ src/scripts/EnventControl.js
@@ -1,3 +1,17 @@
-import {create}
+import { createNewProject } from "./Project"; 
 
 // Control all buttons/events from here
+
+function initializeListeners() {
+
+    // New Project button init
+    const newProjectButton = document.querySelector('.new-project-button');
+    newProjectButton.addEventListener('click', function() {
+        console.log('Hello there Markus!');
+    })
+
+
+
+}
+
+export { initializeListeners }
