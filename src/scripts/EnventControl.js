5c3017ba39574e16289ea8a37dbd8265b2e66313
Markus <markus.h.jarvis@gmail.com>
2022-10-18 20:32:05 -0500

    Rework color guide for a dark theme; all colors updated

--- src/scripts/EnventControl.js
+++ src/scripts/EnventControl.js
@@ -7,6 +7,7 @@ function initializeListeners() {
     const newProjectButton = document.querySelector('.new-project-button');
     const addButton = document.querySelector('.add-button');
     const cancelButton = document.querySelector('.cancel-button');
+    const individualProjects = document.querySelectorAll('project-container a');
     
     // New Project Button
     newProjectButton.addEventListener('click', () => {
@@ -24,10 +25,22 @@ function initializeListeners() {
     cancelButton.addEventListener('click', () => {
         toggleForm();
     })
-
-
-
 }
+
+/*     // Individual Projects
+    individualProjects.forEach(proj => {
+        proj.addEventListener('click', function handleClick(event) {
+            console.log("tst", event);
+        });
+    }) */
+
+
+/*     individualProjects.addEventListener('click', () => {
+        console.log("working");
+    })
+
+} */
+
 
 
 
